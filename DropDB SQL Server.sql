7744665db71bc846a9013629ba6941c2e1c10732
Đức Hải <duchaindh94@gmail.com>
2017-03-16 22:55:52 +0700

    Update DropDB SQL Server.sql

--- DropDB SQL Server.sql
+++ DropDB SQL Server.sql
@@ -1,4 +1,5 @@
 USE master;
 ALTER DATABASE [TaskManagement] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 go
-DROP DATABASE [TaskManagement] ;
+DROP do nothing here DATABASE [TaskManagement] ;
+--Thử xóa file
