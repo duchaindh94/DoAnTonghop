2b9542195340c3d0fd60d5e25677f6f52c58546f
Đức Hải <duchaindh94@gmail.com>
2017-03-16 23:13:12 +0700

    Chỉnh sửa xem có conflict hay không? - Đã conflict sau update rồi commit

--- DropDB SQL Server - Desktop.sql
+++ DropDB SQL Server - Desktop.sql
@@ -1,5 +1,5 @@
 USE master;
 ALTER DATABASE [TaskManagement] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 go
-DROP chỉnh sửa từ Cloud DATABASE [TaskManagement] ;
+DROP do nothing here chỉnh sửa từ ToroiseSVN DATABASE [TaskManagement] ;
 --Thử xóa file
