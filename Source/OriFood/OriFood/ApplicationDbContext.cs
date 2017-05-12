eb7a55f7d3d4302a499d1edd55deef5d31cc46b1
Đức Hải <duchaindh94@gmail.com>
2017-05-12 19:24:06 +0700

    Revert
    Revert

--- Source/OriFood/OriFood/ApplicationDbContext.cs
+++ Source/OriFood/OriFood/ApplicationDbContext.cs
@@ -20,9 +20,20 @@ namespace OriFood
         public virtual DbSet<District> Districts { get; set; }
         public virtual DbSet<Province> Provinces { get; set; }
         public virtual DbSet<ApplicationUser> Users { get; set; }
-        public virtual DbSet<Ward> Wards { get; set; }
+        public virtual DbSet<Precinct> Precincts { get; set; }
         public virtual DbSet<Gender> Genders { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<Album> Albums { get; set; }
+        //public virtual DbSet<AlbumComment> AlbumComments { get; set; }
+        //public virtual DbSet<AlbumLike> AlbumLikes { get; set; }
+        //public virtual DbSet<Category> Categories { get; set; }
+        //public virtual DbSet<Image> Images { get; set; }
+        //public virtual DbSet<PostCategory> PostCategorys { get; set; }
+        //public virtual DbSet<PostComment> PostComments { get; set; }
+        //public virtual DbSet<PostLike> PostLikes { get; set; }
+        //public virtual DbSet<Report> Reports { get; set; }
+        //public virtual DbSet<SavePost> SavePosts { get; set; }
+
 
     }
 }
